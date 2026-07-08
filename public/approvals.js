995794a3bff1ee96b51ea6169dbbb3ce524e557d
sailajasuprava <sailaja.suprava@gmail.com>
2026-07-08 19:09:31 +0530

    Badge  fix 5

--- public/approvals.js
+++ public/approvals.js
@@ -121,7 +121,7 @@ var ApprovalsShared = (function () {
     if (!count) return iconDataUri;
 
     var display = count > 9 ? "9+" : String(count);
-    var fontSize = display.length > 1 ? 15 : 17;
+    var fontSize = display.length > 1 ? 17 : 19;
 
     var svg =
       '<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32">' +
